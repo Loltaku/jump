dc64d946abfbab2cb66d228e8962a2e8aee9b5d4
Loltaku <gongzhudianxiammd@163.com>
2025-03-25 23:40:56 +0800

    我自己写的普通跳跃睡觉了

--- script/player.gd
+++ script/player.gd
@@ -15,14 +15,7 @@ func _physics_process(delta: float) -> void:
 
 	#处理跳跃
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump_press_timer.start()
-		# 跳跃长按检测
-	func _on_jump_press_timer_timeout() -> void:
-		JUMP_PRESS_TIMER = true
-			if JUMP_PRESS_TIMER = true:
-				velocity.y = BIG_JUMP_VELOCITY
-			else:
-				velocity.y = SMALL_JUMP_VELOCITY
+		velocity.y = SMALL_JUMP_VELOCITY
 	
 	#处理移动
 	var direction := Input.get_axis("ui_left", "ui_right")
