c03e4febe2421f68599ea2e999a3f0b70ba05ea4
Loltaku <gongzhudianxiammd@163.com>
2025-03-25 20:32:49 +0800

    加入jumptimer

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-
-const JUMP_VELOCITY = -300.0
+const SPEED = 100.0
+const BIG_JUMP_VELOCITY = -300.0
+const SMALL_JUMP_VELOCITY = -150.0
 
 func _physics_process(delta: float) -> void:
 	# 添加重力
@@ -10,6 +11,13 @@ func _physics_process(delta: float) -> void:
 
 	#处理跳跃
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if.y = SMALL_JUMP_VELOCITY
+	
+	#处理移动
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
