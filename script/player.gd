9dfd640ecc14a9198002b32c5a88dc0e002f788c
Loltaku <gongzhudianxiammd@163.com>
2025-03-25 23:32:50 +0800

    错的，但是是我自己想的

--- script/player.gd
+++ script/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+@onready var jump_press_timer: Timer = $JumpPressTimer
+
 const SPEED = 100.0
 const BIG_JUMP_VELOCITY = -300.0
 const SMALL_JUMP_VELOCITY = -150.0
+
+var JUMP_PRESS_TIMER = false
 
 func _physics_process(delta: float) -> void:
 	# 添加重力
@@ -11,7 +15,14 @@ func _physics_process(delta: float) -> void:
 
 	#处理跳跃
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if.y = SMALL_JUMP_VELOCITY
+		jump_press_timer.start()
+		# 跳跃长按检测
+	func _on_jump_press_timer_timeout() -> void:
+		JUMP_PRESS_TIMER = true
+			if JUMP_PRESS_TIMER = true:
+				velocity.y = BIG_JUMP_VELOCITY
+			else:
+				velocity.y = SMALL_JUMP_VELOCITY
 	
 	#处理移动
 	var direction := Input.get_axis("ui_left", "ui_right")
