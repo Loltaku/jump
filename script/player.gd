643c92cf9f596661f8624de43ae9f3de74f5c35f
Loltaku <gongzhudianxiammd@163.com>
2025-03-26 19:49:07 +0800

    土狼时间：0.039（似乎有bug但管不了那么多了

--- script/player.gd
+++ script/player.gd
@@ -1,22 +1,33 @@
-extends CharacterBody2D
+extends CharacterBody2D  
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -239.0
+const SPEED = 100.0  
+const JUMP_VELOCITY = -239.0  
+const COYOTE_DURATION: float = 0.039  # 土狼时间
 
-func _physics_process(delta: float) -> void:
-	# 添加重力
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var coyote_time_counter: float = 0.000  # 土狼时间计数器  
+var can_jump: bool = true  # 跟踪是否可以跳跃  
 
-	#处理跳跃
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	#处理移动
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(delta: float) -> void:  
+	# 应用重力  
+	if not is_on_floor():  
+		velocity += get_gravity() * delta  
+		coyote_time_counter += delta  # 更新土狼时间计数器  
+	else:  
+		# 落地后处理  
+		if coyote_time_counter <= COYOTE_DURATION:  
+			can_jump = true  # 允许跳跃  
+		coyote_time_counter = 0.000  # 重置计数器  
 
-	move_and_slide()
+	handle_jump()  
+	handle_movement()  
+	move_and_slide()  
+
+# 移动  
+func handle_movement():  
+	velocity.x = Input.get_axis("ui_left", "ui_right") * SPEED  
+
+# 跳跃  
+func handle_jump():  
+	if Input.is_action_just_pressed("jump") and can_jump:  
+		velocity.y = JUMP_VELOCITY  # 执行跳跃  
+		can_jump = false  # 跳跃后设置为不可跳跃  
